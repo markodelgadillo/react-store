1639fa644251521d01c133e484e7e4d2f7973a12
marko delgadillo <markoneseven@gmail.com>
2019-01-08 22:17:45 -0800

    #1 Work from previous exercises.

---
+++ catch-of-the-day/src/components/Router.js
@@ -0,0 +1,20 @@
+// importing React becasuse JSX is being used here
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import StorePicker from './StorePicker'
+import App from './App'
+import NotFound from './NotFound'
+
+
+const Router = () => (
+    <BrowserRouter>
+        <Switch>
+            {/* Each Route tag indicates which path routes to which component */}
+            <Route exact path="/" component={StorePicker} />
+            <Route exact path="/store/:storeid" component={App} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Router;
