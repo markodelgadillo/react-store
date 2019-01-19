cb5b0ad8be690dbbc7d578e50434e99c5e22ff06
marko delgadillo <markoneseven@gmail.com>
2019-01-19 13:26:46 -0800

    #4 Included multiple ways of binding "this" to the component it is in. Commented out the way that uses a method and left the way that uses a property uncommented as the official way of binding this.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,14 +1,44 @@
 import React from "react";
-import { getFunName } from "../helpers.js";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  // declare a property instead of a method
+  // goToStore as a property is not part of the instance of the component
+  // property with an arrow function
+  goToStore = e => {
+    e.preventDefault();
+    console.log(this);
+  };
+
+  //   // method that will run before the storepicke component is created
+  //   constructor() {
+  //     // super() needs to be called first before anything will
+  //     // run in the constructor. It will run the component first -> (React.component)
+  //     super();
+  //     this.goToStore = this.goToStore.bind(this);
+  //   }
+  /* this is the METHOD goToStore()
+   *   that is NOT bound to the instance
+   *   of the component
+   */
+  //   goToStore(e) {
+  //     // 1. Stop the form from submitting
+  //     e.preventDefault();
+  //     // 2. get the test from that input
+  //     console.log(this);
+  //     // 3. changethe page to /store/whatever-they-entered
+  //   }
+
   render() {
     return (
       <React.Fragment>
-        <form action="" className="store-selector">
+        <form action="" className="store-selector" onClick={this.goToStore}>
           <h2>Please Enter a Store</h2>
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Store Name"
             defaultValue={getFunName()}
